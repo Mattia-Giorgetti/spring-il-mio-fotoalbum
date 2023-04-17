b42b105bb8b5f92074de8ecfd13ad3ea5426f2bc
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-04-17 21:28:47 +0200

    fix mail validation

--- src/main/java/org/project/java/springfotoalbum/api/MailRestController.java
+++ src/main/java/org/project/java/springfotoalbum/api/MailRestController.java
@@ -4,7 +4,10 @@ import jakarta.validation.Valid;
 import org.project.java.springfotoalbum.model.Mail;
 import org.project.java.springfotoalbum.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin
@@ -15,7 +18,10 @@ public class MailRestController {
     MailService mailService;
 
     @PostMapping
-    public Mail storeMail(@Valid @RequestBody Mail mail){
+    public Mail storeMail(@Valid @RequestBody Mail mail, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
         return mailService.createMail(mail);
     }
 }
